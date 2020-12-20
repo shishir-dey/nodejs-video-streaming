18e34f361f0917883143a8a73a10501db8c444aa
Shishir Dey <shishir@gvc.in>
2020-12-20 11:17:08 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+  const VP = document.getElementById('videoPlayer')
+  const VPToggle = document.getElementById('toggleButton')
+
+  VPToggle.addEventListener('click', function() {
+    if (VP.paused) VP.play()
+    else VP.pause()
+  })
+}
